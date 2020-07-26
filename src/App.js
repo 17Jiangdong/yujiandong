9bc9c66252dfdbb96dbddf9321e27d5b08358312
17Jiangdong <48679644+17Jiangdong@users.noreply.github.com>
2020-07-26 16:31:56 +0100

    change image

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import doudou_image from './doudou_image.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={doudou_image} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          小猫猫和小狗狗.
         </p>
         <a
           className="App-link"
