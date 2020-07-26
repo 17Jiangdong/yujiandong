89528ec68e3f47306c0e0ad808155f1c79196374
17Jiangdong <48679644+17Jiangdong@users.noreply.github.com>
2020-07-26 15:59:37 +0100

    Updates

---
+++ precache-manifest.5e65afdf75b56bed95184d2b4b0c37d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63009cd2412b2fea288d915f8459aa4a",
+    "url": "/yujiangdong/index.html"
+  },
+  {
+    "revision": "301dfdfe5d878317fdfa",
+    "url": "/yujiangdong/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "14f5d25206bc4b8e3624",
+    "url": "/yujiangdong/static/js/2.46515e64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yujiangdong/static/js/2.46515e64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "301dfdfe5d878317fdfa",
+    "url": "/yujiangdong/static/js/main.c03483aa.chunk.js"
+  },
+  {
+    "revision": "8da3784653ef2361611a",
+    "url": "/yujiangdong/static/js/runtime-main.582ad51e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/yujiangdong/static/media/logo.5d5d9eef.svg"
+  }
+]);
